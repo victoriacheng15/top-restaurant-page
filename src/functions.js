6a1191e1f59653d6fc438b73978afc7c052ad5f1
Victoria Cheng <victoria.cheng15@gmail.com>
2021-10-29 18:18:37 -0400

    clean up

--- src/functions.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-return-assign */
-const createNewElement = (tag) => document.createElement(tag)
-const addId = (element, idName) => element.id = idName
-const addClass = (element, className) => element.classList.add(className)
-const addTextContent = (element, text) => element.textContent = text
-
-export { createNewElement, addId, addClass, addTextContent }
